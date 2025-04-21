6870c2e787038815422091b486d56cf9d579eb90
Nightmare <137179116+NightmareXD0001@users.noreply.github.com>
2025-04-21 11:23:36 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/matrix-mindcraft">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/trivia" element={<Trivia />} />
